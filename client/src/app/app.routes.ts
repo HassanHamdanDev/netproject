7d567396fb0b69e9ee35f13523a48011bc75e7ff
HassanHamdanDev <hassanhamdandev@gmail.com>
2024-12-26 20:48:39 +0000

    set up routing

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { ShopComponent } from './features/shop/shop.component';
+import { ProductsDetailsComponent } from './features/shop/products-details/products-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'shop', component: ShopComponent },
+  { path: 'shop/:id', component: ProductsDetailsComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
